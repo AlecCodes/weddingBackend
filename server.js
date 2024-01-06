20d5c171ae97061f8fdfb3b3f7570e3d90362b82
Alec Hannaford <alec322@gmail.com>
2024-01-06 13:30:11 -0800

    post route works

--- server.js
+++ server.js
@@ -8,4 +8,14 @@ const RSVP = require('./models/rsvp')
 //middleware
 app.use(express.json())
 
+//controller
+app.get("/", async (req,res) => {
+    res.json( await RSVP.find({}))
+})
+
+app.post("/" , async (req,res) => {
+    const newRsvp =  await RSVP.create(req.body)
+    res.json(newRsvp)
+})
+
 app.listen(PORT, () => console.log('LISTENING ON ' + PORT))
