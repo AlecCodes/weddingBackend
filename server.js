aae4d84032272b49856ca4c87af005cbcb0d9968
Alec Hannaford <alec322@gmail.com>
2024-01-06 13:47:27 -0800

    cors middleware

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
+const cors = require('cors')
 require('dotenv').config()
 const RSVP = require('./models/rsvp')
 
 
 //middleware
+app.use(cors())
 app.use(express.json())
 
 //controller
